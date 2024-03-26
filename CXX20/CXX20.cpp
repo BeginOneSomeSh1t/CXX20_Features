e4a11e7651180ee25990cb3f689c69dbdf581788
Wiam <meganite647@gmail.com>
2024-03-26 10:05:13 +0400

    Adding type deduction guidelines for std::pair

--- CXX20/CXX20.cpp
+++ CXX20/CXX20.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
-#include "Source/dummy_array.hpp"
+#include <vector>
+#include <string>
+
+namespace std
+{
+    template<typename T>
+    pair(T&&, const char*) -> pair<T, std::string>;
+
+    template<typename T>
+    pair(const char*, T&&) -> pair<std::string, T>;
+
+    pair(const char*, const char*) -> pair<std::string, std::string>;
+}
+
+
 
 int main(int argc, char* argv[])
 {
-  dummy_array<int, 3> arr;
-    arr.set_at(0, 6);
-    arr.set_at(1, 7);
-    arr.set_at(2, 8);
-    for (auto i : arr) {
-        std::cout << i << std::endl;
-    }
+
+    auto p{std::pair(42, "Hello World!")};
+    
+    std::cout << typeid(p).name() << std::endl;
+    return 0;
 }
