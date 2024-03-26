431dffab26796a6e692a120ebc6c300de6bb4fae
Wiam <meganite647@gmail.com>
2024-03-26 10:47:14 +0400

    std::numeric_limits<T> use cases

--- CXX20/CXX20.cpp
+++ CXX20/CXX20.cpp
@@ -1,25 +1,35 @@
+#include <iomanip>
 #include <iostream>
-#include <vector>
 #include <string>
+#include <limits>
+#include <sstream>
 
-namespace std
+template<typename T>
+auto serialize(T&& value)
 {
-    template<typename T>
-    pair(T&&, const char*) -> pair<T, std::string>;
-
-    template<typename T>
-    pair(const char*, T&&) -> pair<std::string, T>;
-
-    pair(const char*, const char*) -> pair<std::string, std::string>;
+    std::ostringstream oss;
+    oss << std::setprecision(std::numeric_limits<T>::digits10) << value;
+    return oss.str();
 }
-
 
 
 int main(int argc, char* argv[])
 {
+    auto value {1.23456789012345};
+    std::cout << std::setprecision(std::numeric_limits<decltype(value)>::digits10) << value << '\n'; // make sure we show the double at full
 
-    auto p{std::pair(42, "Hello World!")};
+    auto value2{0};
+    std::cin >> value2;
+
+    if(std::cin.fail() || std::to_string(value2).length() > std::numeric_limits<decltype(value2)>::digits10)
+    {
+        std::cout << "Invalid input" << std::endl;
+        return 0;
+    }
+
+    std::cout << std::setprecision(std::numeric_limits<decltype(value2)>::digits10) << value2 << '\n';
+
+    std::cout << serialize(4.56f) << '\n';
+
     
-    std::cout << typeid(p).name() << std::endl;
-    return 0;
 }
