520a632e6371b5d1a25999bdf0c707f7f70de467
Wiam <meganite647@gmail.com>
2024-03-28 08:19:13 +0400

    Add split pattern for the result type

--- CXX20/CXX20.cpp
+++ CXX20/CXX20.cpp
@@ -7,5 +7,10 @@ int main(int argc, char* argv[])
     using namespace std::string_literals;
     auto email_to_test {"PARAMA647@LOCALHOST.com"s};
 
-    std::cout << std::boolalpha << is_valid_format(email_validation_pattern, email_to_test) << std::endl;
+    auto[result, localpart, hostname, dnslable] = is_valid_email_format_with_result(split_email_validation_patter, email_to_test);
+
+    std::cout << "Result: " << result << std::endl;
+    std::cout << "Localpart: " << localpart << std::endl;
+    std::cout << "Hostname: " << hostname << std::endl;
+    std::cout << "Dnslable: " << dnslable << std::endl;
 }
--- CXX20/Source/regex_patterns.hpp
+++ CXX20/Source/regex_patterns.hpp
@@ -7,17 +7,37 @@ namespace regex_patterns
      template<typename CharT>
      using tstring = std::basic_string<CharT, std::char_traits<CharT>, std::allocator<CharT>>;
 
+     using namespace std::string_literals;
+
+
      template<typename CharT>
      bool is_valid_format(tstring<CharT> const& pattern, tstring<CharT> const& text)
      {
           auto rx = std::basic_regex<CharT>{
                pattern, std::regex_constants::icase
           };
-
           return std::regex_match(text, rx);
      }
 
+     template<typename CharT>
+     auto is_valid_email_format_with_result(tstring<CharT> const& pattern, tstring<CharT> const& text)
+     {
+          auto rx = std::basic_regex<CharT>{
+               pattern, std::regex_constants::icase
+          };
+
+          auto result = std::smatch{};
+          auto success = std::regex_match(text, result, rx);
+
+          return std::make_tuple(
+          success,
+          success ? result[1].str() : ""s,
+          success ? result[2].str() : ""s,
+          success ? result[3].str() : ""s
+          );
+     }
+
     // constants
-    using namespace std::string_literals;
     static const auto email_validation_pattern = R"(^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$)"s;
+    static const auto split_email_validation_patter =  R"(^([A-Z0-9._%+-]+)@([A-Z0-9.-]+)\.([A-Z]{2,})$)"s;
 }
