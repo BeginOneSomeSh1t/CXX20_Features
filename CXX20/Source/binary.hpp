05ceb49d0d58c619e76d72a69879845fd8c18137
Wiam <meganite647@gmail.com>
2024-03-30 10:24:35 +0400

    Update binary implementation usign more verbose and expressive constexpr if instead of templated meta programming techniques

--- CXX20/Source/binary.hpp
+++ CXX20/Source/binary.hpp
@@ -10,53 +10,39 @@ namespace binary
     {
         namespace binary_literals_internals
         {
-            template<typename  CharT, char... bits>
-            struct binary_struct;
-
-            template<typename CharT, char... bits>
-            struct binary_struct<CharT, '0', bits...>
-            {
-                static constexpr CharT value
-                {
-                    binary_struct<CharT, bits...>::value
-                };
-            };
-
-            template<typename CharT, char... bits>
-            struct binary_struct<CharT, '1', bits...>
-            {
-                static constexpr CharT value{
-                    static_cast<CharT>(1 << sizeof...(bits)) |
-                    binary_struct<CharT, bits...>::value
-                };
-            };
-
-            template<typename CharT>
-            struct binary_struct<CharT>
-            {
-                static constexpr CharT value{0};
-            };
+           template<typename CharT, char d, char... bits>
+            constexpr CharT binary_eval()
+           {
+               if constexpr(sizeof...(bits) == 0)
+                   return static_cast<CharT>(d-'0');
+               else if constexpr (d == '0')
+                   return binary_eval<CharT, bits...>();
+               else if constexpr (d == '1')
+                   return static_cast<CharT>(
+                    (1 << sizeof...(bits)) | binary_eval<CharT, bits...>()
+                   );
+           }
         }
 
         template<char...bits>
         constexpr byte8 operator""_b8()
         {
             static_assert(sizeof...(bits) <= 8, "Too many bits");
-            return binary_literals_internals::binary_struct<byte8, bits...>::value;
+            return binary_literals_internals::binary_eval<byte8, bits...>();
         }
 
         template<char... bits>
         constexpr byte16 operator""_b16()
         {
             static_assert(sizeof...(bits) <= 16, "Too many bits");
-            return binary_literals_internals::binary_struct<byte16, bits...>::value;
+            return binary_literals_internals::binary_eval<byte16, bits...>();
         }
         
         template<char... bits>
         constexpr byte32 operator""_b32()
         {
             static_assert(sizeof...(bits) <= 32, "Too many bits");
-            return binary_literals_internals::binary_struct<byte32, bits...>::value;
+            return binary_literals_internals::binary_eval<byte32, bits...>();
         }
     }
 }
