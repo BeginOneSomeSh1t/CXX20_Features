6f1ea6fe7071dea07272f4eacf1e2ccccbf70cf9
Wiam <meganite647@gmail.com>
2024-03-30 09:22:22 +0400

    Introduce apply function for callable objects invocation

---
+++ CXX20/Source/functions.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <utility>
+
+namespace details
+{
+    template<typename F, typename T, std::size_t... I>
+    auto apply(F&& f, T&& t, std::index_sequence<I...>)
+    {
+        return std::invoke(
+            std::forward<F>(f),
+            std::get<I>(std::forward<T>(t))...
+        );
+    }
+    
+}
+
+
+template<typename F, typename T>
+auto apply(F&& f, T&& t)
+{
+    return details::apply(
+        std::forward<F>(f),
+        std::forward<T>(t),
+        std::make_index_sequence<std::tuple_size_v<std::decay_t<T>>>{}
+    );
+}
