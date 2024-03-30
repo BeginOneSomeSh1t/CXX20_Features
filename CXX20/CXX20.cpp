7578ebf394206ae935163a535bbdb24a3505947a
Wiam <meganite647@gmail.com>
2024-03-30 16:37:11 +0400

    Generating my own hash for a custom type

--- CXX20/CXX20.cpp
+++ CXX20/CXX20.cpp
@@ -1,21 +1,65 @@
+#include <algorithm>
 #include <iostream>
-#include <string>
+#include <unordered_set>
+#include "Source/time.hpp"
+using namespace std::string_literals;
 
-#include "Source/compose.hpp"
+struct Item
+{
+    int id;
+    std::string name;
+    double value;
+
+    Item(int const id, std::string const& name, double const value)
+        :
+    id(id), name(name), value(value)
+    {}
+
+    bool operator==(const Item& other) const
+    {
+        return id == other.id && name == other.name && value == other.value;
+    }
+
+    // Overload the '<<' operator for Item
+    friend std::ostream& operator<<(std::ostream& os, const Item& item) {
+        os << "ID: " << item.id << ", Name: " << item.name << ", Value: " << item.value;
+        return os;
+    }
+};
+
+namespace std
+{
+    template<>
+    struct hash<Item>
+    {
+        typedef Item argument_type;
+        typedef size_t result_type;
+
+        result_type operator()(argument_type const& item) const
+        {
+            result_type hash_val = 17;
+            hash_val = 31 * hash_val + std::hash<int>()(item.id);
+            hash_val = 31 * hash_val + std::hash<std::string>()(item.name);
+            hash_val = 31 * hash_val + std::hash<double>()(item.value);
+
+            return hash_val;
+        }
+    };
+}
 
 
 int main(int argc, char* argv[])
 {
+    std::unordered_set<Item> set{
+        {1, "one", 1.0},
+        {2, "two", 2.0},
+        {3, "three", 3.0},
+    };
 
+    for (const Item& item : set) {
+        std::cout << item << std::endl;
+    }
 
-    using namespace compose;
-    
-    auto n =
-    ([](int const n){return std::to_string(n);} *
-    [](int const n) {return n*n;} *
-    [](int const n) {return n + n;} *
-    [](int const n) {return std::abs(n);});
-
-    std::cout << n(3);
+    return 0;
 }
 
---
+++ CXX20/Source/time.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <chrono>
+
+template<typename Time = std::chrono::microseconds,
+         typename Clock = std::chrono::high_resolution_clock>
+struct performance_timer
+{
+    template<typename F, typename... Ts>
+    static Time duration(F&& f, Ts... rest)
+    {
+        auto start = Clock::now();
+        std::invoke(std::forward<F>(f), std::forward<Ts>(rest)...);
+        auto end = Clock::now();
+
+        return std::chrono::duration_cast<Time>(end - start);
+    }
+};
