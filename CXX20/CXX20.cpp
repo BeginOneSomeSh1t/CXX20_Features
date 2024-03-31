31dfe7662af9ec382bc6c140ca3d8f88a2ae5b9f
Wiam <meganite647@gmail.com>
2024-03-31 14:41:51 +0400

    Write a producer-consumer model using simple multithreading techniques

--- CXX20/CXX20.cpp
+++ CXX20/CXX20.cpp
@@ -4,6 +4,10 @@
 #include <chrono>
 #include <thread>
 #include <mutex>
+#include <exception>
+#include <condition_variable>
+#include <queue>
+#include <random>
 
 inline void print_time()
 {
@@ -12,47 +16,6 @@ inline void print_time()
     auto ltime{ std::localtime(&stime) };
 
     std::cout << std::put_time(ltime, "%c") << '\n';
-}
-
-std::mutex g_mutex;
-
-void thread_func()
-{
-    using namespace std::chrono_literals;
-    {
-        std::lock_guard lock{g_mutex};
-        std::cout << "Running thread " << std::this_thread::get_id() << '\n';
-    }
-
-    std::this_thread::yield();
-    std::this_thread::sleep_for(2s);
-
-    {
-        std::lock_guard lock{g_mutex};
-        std::cout << "Done in thread " << std::this_thread::get_id() << '\n';
-    }
-}
-
-template<typename T>
-struct container
-{
-    std::mutex     mutex;
-    std::vector<T> data;
-};
-
-template<typename T>
-void move_between(container<T>& c1, container<T>& c2, T const value)
-{
-    std::lock(c1.mutex, c2.mutex);
-
-    std::lock_guard l1{c1.mutex, std::adopt_lock};
-    std::lock_guard l2{c2.mutex, std::adopt_lock};
-
-    c1.data.erase(
-        std::remove(c1.data.begin(), c1.data.end(), value),
-        c1.data.end()
-    );
-    c2.data.push_back(value);
 }
 
 template<typename R>
@@ -65,53 +28,101 @@ void print_range(R&& r)
     std::cout << '\n';
 }
 
+std::mutex g_lockprint;
+std::mutex g_lockqueue;
+std::condition_variable g_queuecheck;
+std::queue<int> g_buffer;
+bool g_done;
 
+void producer(
+     int const                           id,
+     std::mt19937&                       generator,
+     std::uniform_int_distribution<int>& dsleep,
+     std::uniform_int_distribution<int>& dcode
+)
+{
+    for (int i = 0; i < 5; ++i)
+    {
+        // simulate work
+        std::this_thread::sleep_for(
+        std::chrono::seconds(dsleep(generator))
+        );
+    }
+
+    // generate data
+    {
+        std::unique_lock lock{ g_lockqueue };
+        const int value{ id * 100 + dcode(generator) };
+        g_buffer.push(value);
+
+        {
+            std::unique_lock io_lock{ g_lockprint };
+            std::cout << std::format("[produced({})]: {} \n", id, value) << std::flush;
+        }
+    }
+
+    // notify consumers
+    g_queuecheck.notify_one();
+}
+
+void consumer()
+{
+    // loop untill end is signaled
+    while(!g_done)
+    {
+        std::unique_lock lock{ g_lockqueue };
+
+        g_queuecheck.wait_for(
+            lock,
+            std::chrono::seconds{ 1 },
+            [&]{ return !g_buffer.empty(); }
+        );
+
+        while(!g_done && !g_buffer.empty())
+        {
+            std::unique_lock print_lock{ g_lockprint };
+            std::cout << std::format("[consumed]: {} \n", g_buffer.front()) << std::flush;
+            g_buffer.pop();
+        }
+    }
+}
 
 int main(int argc, char* argv[])
 {
+    std::array<int, std::mt19937::state_size> seed_data;
+    std::random_device rd;
+    std::generate(seed_data.begin(), seed_data.end(), std::ref(rd));
+
+    std::seed_seq seq{ seed_data.begin(), seed_data.end() };
+    std::mt19937 generator{ seq };
+    std::uniform_int_distribution<> dsleep{ 1, 5 };
+    std::uniform_int_distribution<> dcode{ 1, 99 };
+
+    std::cout << "Start producing and consuming..." << std::flush << '\n';
+
+    std::thread consumer_thread{ consumer };
     std::vector<std::thread> threads;
     for (int i = 0; i < 5; ++i)
     {
-        threads.emplace_back(thread_func);
+        threads.emplace_back(
+                producer,
+                i + 1,
+                std::ref(generator),
+                std::ref(dsleep),
+                std::ref(dcode)
+        );
     }
 
-    for (auto& thread : threads)
+    // work for the workers to finish
+    for (auto& t : threads)
     {
-        thread.join();
+        t.join();
     }
 
+    // notify the looger to finish and wait for it
+    g_done = true;
+    consumer_thread.join();
 
-    container<int> c1;
-    c1.data.push_back(1);
-    c1.data.push_back(2);
-    c1.data.push_back(3);
-    c1.data.push_back(4);
-
-    std::cout << "c1 before: \n";
-    print_range(c1.data);
-
-    container<int> c2;
-    c2.data.push_back(5);
-    c2.data.push_back(6);
-    c2.data.push_back(7);
-    c2.data.push_back(8);
-
-    std::cout << "c2 before: \n";
-    print_range(c2.data);
-
-
-    std::cout << "Moving between...." << '\n';
-
-    std::thread t1{move_between<int>, std::ref(c1), std::ref(c2), 3};
-    std::thread t2{move_between<int>, std::ref(c2), std::ref(c1), 6};
-
-    t1.join();
-    t2.join();
-
-    std::cout << "c1 after: \n";
-    print_range(c1.data);
-    
-    std::cout << "c2 after: \n";
-    print_range(c2.data);
+    std::cout << "done producing and consuming...";
 }
 
